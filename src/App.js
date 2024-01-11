ab22b5f152f242ef3b0e5adec6db04aad51a4920
hansol <hansol8701@naver.com>
2024-01-11 22:17:59 +0900

    챕터2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from "styled-components";
+
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    background-color: ${(props) => props.theme.backgroundColor};
+`;
+
+const Text = styled.span`
+    font-size: 50px;
+    color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Container>
+            <Text>Hello</Text>
+        </Container>
+    );
 }
 
 export default App;
